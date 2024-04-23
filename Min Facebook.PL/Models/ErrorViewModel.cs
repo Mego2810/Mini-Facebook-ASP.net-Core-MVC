e468159e56e82133879f7aba20a6f5d8c0762589
pc <pc@AHMEDMAGDY>
2024-04-23 17:44:25 +0200

    Add project files.

---
+++ Min Facebook.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Min_Facebook.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
